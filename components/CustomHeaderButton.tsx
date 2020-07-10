852071b747ab4b44417d57a2553ed2fc44f710a5
Ayush Goyal <goyalayu@amazon.com>
2020-07-10 10:59:08 +0530

    Add custom header button

---
+++ components/CustomHeaderButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  HeaderButton,
+  HeaderButtonProps,
+} from 'react-navigation-header-buttons';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import Colors from '../constants/Colors';
+
+interface CustomHeaderButtonProps extends HeaderButtonProps {}
+
+const CustomHeaderButton: React.FC<CustomHeaderButtonProps> = (props) => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Ionicons}
+      iconSize={23}
+      color={Platform.OS === 'android' ? 'white' : Colors.primaryColor}
+    />
+  );
+};
+
+export default CustomHeaderButton;
--- screens/MealDetailScreen.tsx
+++ screens/MealDetailScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const MealDetailScreen: NavigationStackScreenComponent = (props) => {
   const mealId = props.navigation.getParam('mealId');
@@ -27,6 +29,15 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => console.log('yolo')}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
